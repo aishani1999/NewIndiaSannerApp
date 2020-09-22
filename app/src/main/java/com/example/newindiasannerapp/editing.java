14ad94a382dfac77d9bc8ca839e5402430db0ea6
aishani1999 <aishani.mukerji@gmail.com>
2020-09-23 01:30:42 +0530

    filter complete

--- app/src/main/java/com/example/newindiasannerapp/editing.java
+++ app/src/main/java/com/example/newindiasannerapp/editing.java
@@ -1,12 +1,5 @@
 package com.example.newindiasannerapp;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
@@ -14,97 +7,36 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+public class editing extends AppCompatActivity {
 
-    ImageButton bb1;
-    Button bt_bw,bb2,bt_magic;
-    ImageView iv;
-    Uri uri;
     double value=50;
     double magic_value=60;
+    Button bt_bw,bb2,bt_magic;
+    ImageView iv1;
+    Uri uri;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        bb1=findViewById(R.id.bt_image);
         bb2=findViewById(R.id.bt_gray);
         bt_bw=findViewById(R.id.bt_bw);
         bt_magic=findViewById(R.id.bt_magic);
-        iv=findViewById(R.id.image_view);
-
-    }
-
-    public  void onhooseFile(View v){
-        CropImage.activity().start(MainActivity.this);
-
-    }
-    public void grayView(View view)
-    {
-
-        try {
-            Bitmap mbitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
-            Bitmap mimage = doGreyscale(mbitmap);
-            iv.setImageBitmap(mimage);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public void bwView(View view)
-    {
-
-        try {
-            Bitmap mbitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
-            Bitmap bw_image = doBlackAndWhite(mbitmap,value);
-            iv.setImageBitmap(bw_image);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void magicView(View view)
-    {
-
-        try {
-            Bitmap mbitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
-            Bitmap magic_image = doMagic(mbitmap,magic_value);
-            iv.setImageBitmap(magic_image);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        iv1=findViewById(R.id.image_view);
+        Bundle extras = getIntent().getExtras();
+        uri = Uri.parse(extras.getString("KEY"));
+        iv1.setImageURI(uri);
     }
 
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
-            CropImage.ActivityResult result=CropImage.getActivityResult(data);
-            if(resultCode== RESULT_OK){
-                uri=result.getUri();
-                try {
-                    Bitmap mbitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                iv.setImageURI(uri);
-            }
-            else if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
-                Exception e= result.getError();
-                Toast.makeText(this,"Possible error is "+e,Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
 
     public static Bitmap doGreyscale(Bitmap src) {
         // constant factors
@@ -227,4 +159,18 @@ public class MainActivity extends AppCompatActivity {
         // return final image
         return bmOut;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
